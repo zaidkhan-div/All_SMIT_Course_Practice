09d554b4abf6fd2934f21263994fc0a76c44c6da
KhanzaidGithub <zaidscestudent@gmail.com>
2024-12-07 09:52:54 -0800

    Started Learning Array At SMIT

---
+++ Javascript/Array/app.js
@@ -0,0 +1,34 @@
+// var age = +prompt("Enter your age?");
+
+// if (age >= 18) {
+//   console.log("You are adult");
+// } else if (age >= 13) {
+//   console.log("You are a teenager");
+// } else {
+//   console.log("You are a child");
+// }
+
+// if (age >= 18) {
+//   console.log("You are adult");
+// } else {
+
+//   if (age >= 13) {
+//     console.log("You are a teenager");
+//   } else {
+//     console.log("You are a child");
+//   }
+
+// }
+
+// var student1 = "Ali";
+// var student2 = "Aslam";
+// var student3 = "Atif";
+// console.log(student1, student2, student3);
+
+// array   []
+// 0 1 2 3 4 5 6
+
+// Index  0 1 2 3
+var students = ["Ali", "Aslam", "Atif"];
+students[3] = "Huzaifa";
+console.log(students);
