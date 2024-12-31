5577b2c3507e954166583d9e52a1f13c868fdf10
KhanzaidGithub <zaidscestudent@gmail.com>
2024-12-31 09:49:05 -0800

    Dice Gam

---
+++ Javascript/If-Else-Method/app.js
@@ -0,0 +1,86 @@
+// var question = 4 * 2 / 2;
+// var question = (4 * 10) / 5;
+// var question = (4 + 10) * 5;
+// console.log(question);
+
+
+
+// var num1 = 1;
+// num1 = num1 + 1;
+// num1++;
+// num1++;
+// console.log(num1);
+
+
+
+// var number = 10;
+// // number = number - 1;
+// // number--;
+
+// // Post Increment / Post Decrement variable++ || variable--
+// // Pre Increment / Pre Decrement ++variable || --variable
+
+
+
+// console.log(number++);
+
+
+// var firstName = "Ali";
+// var lastName = "Huzaifa";
+// console.log(firstName + " " + lastName);
+// // concatenate
+
+
+// ===
+// agar
+
+// =
+// ===
+
+// var firstName = "Ali";
+
+// if (firstName === "Asad") {
+//     alert("Success");
+// }
+
+
+// var userBirthDayMonth = prompt("Enter Your Birthday Month Name");
+// if (userBirthDayMonth === "August") {
+//     alert("Happy Birthday")
+// }
+
+
+// var userScore = 0;
+// var question = prompt("Who is the founder of spaceX");
+// if (question === "Elon Musk") {
+//     ++userScore;
+//     // userScore = userScore + 1;
+// }
+
+// console.log(userScore);
+
+// var userQuestion = prompt("Enter the color traffic light");
+// if (userQuestion === "Red") {
+//     alert("Must Stop");
+// } else {
+//     alert("Go");
+// }
+
+
+
+// if (1 === 1){
+
+// }
+
+// Right Side Greater Value than left side
+var userAge = +prompt("Enter Your Age");
+// ===
+// >
+// <
+// >=
+// <=
+if (userAge <= 18) {
+    alert("Teenager")
+} else {
+    alert("Adult")
+}
