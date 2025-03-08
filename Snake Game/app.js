e6cd802f5a379d5efd0d69dd92ef51271b8670ee
KhanzaidGithub <zaidscestudent@gmail.com>
2025-03-07 23:00:04 -0800

    .

--- Snake Game/app.js
+++ Snake Game/app.js
@@ -138,17 +138,128 @@ setInterval(function () {
   moveSnake();
 }, 200);
 
-// Get HTML elements
-// Variables
-// DrawElement or DrawGame function
-// food Random Position
-// Move Snake
-// Game Over Conditions
-// SetInterval For continuously creating a new board
+// // Get HTML elements
+// // Variables
+// // DrawElement or DrawGame function
+// // food Random Position
+// // Move Snake
+// // Game Over Conditions
+// // SetInterval For continuously creating a new board
 
-// Important
-// Jaise hi hum Arrow Key press karte hain, direction update ho jata hai.
-// setInterval() function har 200ms baad moveSnake() ko call karta rehta hai.
-// moveSnake() function snake ki new position calculate karta hai:
-// Naya head pichli position + direction ke mutabiq set hota hai.
-// Snake continuously move karta rehta hai jab tak koi naya arrow press na ho.
+// // Important
+// // Jaise hi hum Arrow Key press karte hain, direction update ho jata hai.
+// // setInterval() function har 200ms baad moveSnake() ko call karta rehta hai.
+// // moveSnake() function snake ki new position calculate karta hai:
+// // Naya head pichli position + direction ke mutabiq set hota hai.
+// // Snake continuously move karta rehta hai jab tak koi naya arrow press na ho.
+
+
+
+
+
+// Step 1 Elements Get
+// var board = document.querySelector(".board");
+// var highestScore = document.querySelector(".highest-score");
+// var score = document.querySelector(".score");
+
+// var initialSnakeHead = snakeHeadRandomPosition();
+// var snake = [initialSnakeHead];
+// var direction = { x: 0, y: 0 };
+// var food = foodRandomPosition();
+// var currentScore = 0;
+// var hScore = localStorage.getItem("highest-score");
+
+// score.innerHTML = `Score ${currentScore}`;
+
+// if (hScore) {
+//   highestScore.innerHTML = `Highest Score ${hScore}`;
+// } else {
+//   highestScore.innerHTML = `Highest Score 0`;
+// }
+
+// function foodRandomPosition() {
+//   // 0.1 * 18 === 0.188 === 0
+//   var randomXPosition = Math.floor(Math.random() * 18 + 1);
+//   var randomYPosition = Math.floor(Math.random() * 18 + 1);
+//   return {
+//     x: randomXPosition,
+//     y: randomYPosition,
+//   };
+// }
+
+// function snakeHeadRandomPosition() {
+//   // 0.1 * 18 === 0.188 === 0
+//   var randomXPosition = Math.floor(Math.random() * 18 + 1);
+//   var randomYPosition = Math.floor(Math.random() * 18 + 1);
+//   return {
+//     x: randomXPosition,
+//     y: randomYPosition,
+//   };
+// }
+
+// // position = { x: 10 ,y: 8 }
+// function drawElement(position, className) {
+//   var element = document.createElement("div");
+//   element.style.gridRowStart = position.y;
+//   element.style.gridColumnStart = position.x;
+//   element.classList.add(className);
+//   board.appendChild(element);
+// }
+
+// function drawGame() {
+//   board.innerHTML = "";
+
+//   // Food Draw
+//   drawElement(food, "food");
+
+//   // Snake Draw
+//   for (var i = 0; i < snake.length; i++) {
+//     var item = snake[i];
+//     if (i === 0) {
+//       drawElement(item, "head");
+//     } else {
+//       drawElement(item, "tail");
+//     }
+//   }
+// }
+
+// function moveSnake() {
+//   // 10 , 9
+//   var newHead = { x: snake[0].x + direction.x, y: snake[0].y + direction.y };
+//   snake.unshift(newHead);
+
+//   // Food is eaten
+//   var snakeHead = snake[0];
+//   if (snakeHead.x === food.x && snakeHead.y === food.y) {
+//     food = foodRandomPosition();
+//   } else {
+//     snake.pop();
+//   }
+// }
+
+// window.addEventListener("keydown", function (e) {
+//   const key = e.key;
+//   let newDirection;
+//   if (key === "ArrowUp") {
+//     newDirection = { x: 0, y: -1 };
+//   } else if (key === "ArrowDown") {
+//     newDirection = { x: 0, y: 1 };
+//   } else if (key === "ArrowLeft") {
+//     newDirection = { x: -1, y: 0 };
+//   } else if (key === "ArrowRight") {
+//     newDirection = { x: 1, y: 0 };
+//   }
+
+//   // If NewDirection valid hun then direction ko update kar dena
+
+//   if (newDirection) {
+//     if (newDirection.x !== -direction.x || newDirection.y !== -direction.y) {
+//       direction = newDirection;
+//     }
+//   }
+// });
+
+// setInterval(function () {
+//   drawGame();
+//   moveSnake();
+// }, 200);
