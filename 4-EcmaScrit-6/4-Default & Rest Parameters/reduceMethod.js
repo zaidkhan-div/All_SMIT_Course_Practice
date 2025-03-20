515f0fad3ab025db93daf0479448a1a5fc3d2dea
KhanzaidGithub <zaidscestudent@gmail.com>
2025-03-19 23:30:31 -0700

    Reduce Method

--- 4-EcmaScrit-6/4-Default & Rest Parameters/reduceMethod.js
+++ 4-EcmaScrit-6/4-Default & Rest Parameters/reduceMethod.js
@@ -29,3 +29,5 @@ const flatArray = nestedArray.reduce((acc, arr) => acc.concat(arr), [],);
 
 console.log(flatArray); // [1, 2, 3, 4, 5, 6]
 
+const checking = grades.reduce((acc, num) => acc.concat(num), [])
+console.log(checking)
