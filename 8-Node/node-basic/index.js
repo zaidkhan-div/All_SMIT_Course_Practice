6797eb5eb41f251c4798e67fb7a4c0d3a006e1ce
zaidkhan-div <zaidscestudent@gmail.com>
2025-08-30 13:13:17 +0500

    crud in Express

--- 8-Node/node-basic/index.js
+++ 8-Node/node-basic/index.js
@@ -1,7 +1,32 @@
-import express from "express"
-
+import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 
-console.log("Hello Express!");
+// Middleware
+app.use(bodyParser.urlencoded({ extends: false }));
+// app.use("/", (req, res, next) => {
+//   req.data = "12345678910";
+//   next();
+// });
 
-app.listen(3002);
+// methods
+// get post put delete patch
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/form", (req, res) => {
+  res.send(`
+   <form action='/submit' method="POST">
+        <input name="name" placeholder="Enter Your Name" />
+        <button type="submit">Submit</button>
+        </form> 
+    `);
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body.name);
+  res.send("Data Submitted Successfully");
+});
+
+app.listen(3000);
